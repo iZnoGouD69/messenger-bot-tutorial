62d8bd3fd64d55416d931146fbfce616f381bbfb
iZnoGouD69 <parthsolanki1498@gmail.com>
2016-04-19 15:49:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/webhook/', function (req, res) {
 		sender = event.sender.id
 		if (event.message && event.message.text) {
 			text = event.message.text
-			if (text === 'Mia Khalida') {
+			if (text === 'Mia Khalifa') {
 				sendGenericMessage(sender)
 				continue
 			}
